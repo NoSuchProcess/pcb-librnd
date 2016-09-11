NoSuchProcess/pcb-librnd

commit 224cd9170812cb2937c280a1c879626066387a77

[core] add new conf value line_refraction, rewiring Clipping
git-svn-id: svn://repo.hu/librnd/trunk@3177 1d61de99-560f-4dba-8957-1e505d42358b

src/conf_core.h
@@ -30,6 +30,7 @@ typedef struct {
 		CFT_BOOLEAN highlight_on_point;    /* Highlight if crosshair is on endpoints. */
 		CFT_BOOLEAN show_solder_side;      /* mirror output */
 		CFT_BOOLEAN save_last_command;     /* save the last command entered by user */
+		CFT_INTEGER line_refraction;	   /* value for line lookahead setting */
 		CFT_BOOLEAN save_in_tmp;           /* always save data in /tmp */
 		CFT_BOOLEAN draw_grid;             /* draw grid points */
 		CFT_BOOLEAN all_direction_lines;   /* enable lines to all directions */

commit 865b9eaa30fa6c78a075432f9b71d5b8e3296b15

[core] -Fix: broken indentation in r3177
git-svn-id: svn://repo.hu/librnd/trunk@3178 1d61de99-560f-4dba-8957-1e505d42358b

src/conf_core.h
@@ -30,7 +30,7 @@ typedef struct {
 		CFT_BOOLEAN highlight_on_point;    /* Highlight if crosshair is on endpoints. */
 		CFT_BOOLEAN show_solder_side;      /* mirror output */
 		CFT_BOOLEAN save_last_command;     /* save the last command entered by user */
-		CFT_INTEGER line_refraction;	   /* value for line lookahead setting */
+		CFT_INTEGER line_refraction;       /* value for line lookahead setting */
 		CFT_BOOLEAN save_in_tmp;           /* always save data in /tmp */
 		CFT_BOOLEAN draw_grid;             /* draw grid points */
 		CFT_BOOLEAN all_direction_lines;   /* enable lines to all directions */