NoSuchProcess/pcb-librnd

commit 6f17e6800268ba9cee6d289b07340e5e94b44cb2

[query] -Split: data listing from subc listing so it can be reused for buffer later on
git-svn-id: svn://repo.hu/librnd/trunk@27548 1d61de99-560f-4dba-8957-1e505d42358b

src_plugins/query/query_access.c
@@ -72,13 +72,10 @@ static void list_pstk_cb(void *ctx, pcb_board_t *pcb, pcb_pstk_t *ps)
 	APPEND(ctx, ps);
 }
 
-static int list_subc_cb(void *ctx, pcb_board_t *pcb, pcb_subc_t *subc, int enter)
-{
-	
-	if (enter) {
-		pcb_data_t *data = subc->data;
-		APPEND(ctx, subc);
+static int list_subc_cb(void *ctx, pcb_board_t *pcb, pcb_subc_t *subc, int enter);
 
+static int list_data(void *ctx, pcb_board_t *pcb, pcb_data_t *data, int enter)
+{
 		PCB_SUBC_LOOP(data);
 		{
 			list_subc_cb(ctx, pcb, subc, 1);
@@ -110,11 +107,18 @@ static int list_subc_cb(void *ctx, pcb_board_t *pcb, pcb_subc_t *subc, int enter
 			list_poly_cb(ctx, pcb, layer, polygon);
 		}
 		PCB_ENDALL_LOOP;
-	}
 
 	return 0;
 }
 
+static int list_subc_cb(void *ctx, pcb_board_t *pcb, pcb_subc_t *subc, int enter)
+{
+	if (enter) {
+		APPEND(ctx, subc);
+		list_data(ctx, pcb, subc->data, enter);
+	}
+	return 0;
+}
 
 void pcb_qry_list_all(pcb_qry_val_t *lst, pcb_objtype_t mask)
 {

commit a915fea3061c41f596caae92b47970075a851faa

[query] -Cleanup: indentation broken by r27548
git-svn-id: svn://repo.hu/librnd/trunk@27549 1d61de99-560f-4dba-8957-1e505d42358b

src_plugins/query/query_access.c
@@ -76,37 +76,37 @@ static int list_subc_cb(void *ctx, pcb_board_t *pcb, pcb_subc_t *subc, int enter
 
 static int list_data(void *ctx, pcb_board_t *pcb, pcb_data_t *data, int enter)
 {
-		PCB_SUBC_LOOP(data);
-		{
-			list_subc_cb(ctx, pcb, subc, 1);
-		}
-		PCB_END_LOOP;
-		PCB_PADSTACK_LOOP(data);
-		{
-			list_pstk_cb(ctx, pcb, padstack);
-		}
-		PCB_END_LOOP;
+	PCB_SUBC_LOOP(data);
+	{
+		list_subc_cb(ctx, pcb, subc, 1);
+	}
+	PCB_END_LOOP;
+	PCB_PADSTACK_LOOP(data);
+	{
+		list_pstk_cb(ctx, pcb, padstack);
+	}
+	PCB_END_LOOP;
 
-		PCB_ARC_ALL_LOOP(data);
-		{
-			list_arc_cb(ctx, pcb, layer, arc);
-		}
-		PCB_ENDALL_LOOP;
-		PCB_LINE_ALL_LOOP(data);
-		{
-			list_line_cb(ctx, pcb, layer, line);
-		}
-		PCB_ENDALL_LOOP;
-		PCB_TEXT_ALL_LOOP(data);
-		{
-			list_text_cb(ctx, pcb, layer, text);
-		}
-		PCB_ENDALL_LOOP;
-		PCB_POLY_ALL_LOOP(data);
-		{
-			list_poly_cb(ctx, pcb, layer, polygon);
-		}
-		PCB_ENDALL_LOOP;
+	PCB_ARC_ALL_LOOP(data);
+	{
+		list_arc_cb(ctx, pcb, layer, arc);
+	}
+	PCB_ENDALL_LOOP;
+	PCB_LINE_ALL_LOOP(data);
+	{
+		list_line_cb(ctx, pcb, layer, line);
+	}
+	PCB_ENDALL_LOOP;
+	PCB_TEXT_ALL_LOOP(data);
+	{
+		list_text_cb(ctx, pcb, layer, text);
+	}
+	PCB_ENDALL_LOOP;
+	PCB_POLY_ALL_LOOP(data);
+	{
+		list_poly_cb(ctx, pcb, layer, polygon);
+	}
+	PCB_ENDALL_LOOP;
 
 	return 0;
 }