e74682c9a9ed043c5575382cc2506bd92067d7f8
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-07-14 13:58:19 +0000

    [menu] -Del: old way of handling menu anchors - menu merging took over
    git-svn-id: svn://repo.hu/librnd/trunk@32285 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/hid_menu.h
+++ src/librnd/core/hid_menu.h
@@ -30,15 +30,6 @@ lht_node_t *rnd_hid_cfg_get_menu(rnd_hid_cfg_t *hr, const char *menu_path);
 lht_node_t *rnd_hid_cfg_get_menu_at(rnd_hid_cfg_t *hr, lht_node_t *at, const char *menu_path, lht_node_t *(*cb)(void *ctx, lht_node_t *node, const char *path, int rel_level), void *ctx);
 lht_node_t *rnd_hid_cfg_get_menu_at_node(lht_node_t *at, const char *menu_path, lht_node_t *(*cb)(void *ctx, lht_node_t *node, const char *path, int rel_level), void *ctx);
 
-
-/* search all instances of an @anchor menu in the currently active GUI and
-   call cb on the lihata node; path has 128 extra bytes available at the end */
-void rnd_hid_cfg_map_anchor_menus(const char *name, void (*cb)(void *ctx, rnd_hid_cfg_t *cfg, lht_node_t *n, char *path), void *ctx);
-
-/* remove all adjacent anchor menus with matching cookie below anode, the
-   anchor node */
-int rnd_hid_cfg_del_anchor_menus(lht_node_t *anode, const char *cookie);
-
 /* plugins can manually create dynamic menus using this call; props->cookie
    must be set, the menu patch is identified by that cookie */
 int rnd_hid_menu_create(const char *path, const rnd_menu_prop_t *props);
