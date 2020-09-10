decfb11903fe685d5416c18684acf5a675f6a973
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-09-10 05:42:49 +0000

    -Move: src_plugins to src/librnd/plugins for easier svn externs and consistency with the installation later
    git-svn-id: svn://repo.hu/librnd/trunk@32397 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -14,7 +14,7 @@
 #define version "2.2.4"
 
 #define LIBRND_ROOT         "../src/librnd"
-#define LIBRND_PLUGIN_ROOT  "../src_plugins"
+#define LIBRND_PLUGIN_ROOT  "../src/librnd/plugins"
 #include "../src_3rd/puplug/scconfig_hooks.h"
 #include "../src_3rd/libfungw/scconfig_hooks.h"
 #include "../src_3rd/libporty_net/hooks_net.c"
@@ -255,7 +255,7 @@ int hook_generate()
 
 	printf("Generating pcb-rnd config.h (%d)\n", generr |= tmpasm("..", "config.h.in", "config.h"));
 
-	printf("Generating opengl.h (%d)\n", generr |= tmpasm("../src_plugins/lib_hid_gl", "opengl.h.in", "opengl.h"));
+	printf("Generating opengl.h (%d)\n", generr |= tmpasm("../src/librnd/plugins/lib_hid_gl", "opengl.h.in", "opengl.h"));
 
 	printf("Generating tests/librnd/inc_all.h (%d)\n", generr |= tmpasm("../tests/librnd", "inc_all.h.in", "inc_all.h"));
 
