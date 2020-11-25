f720355a1b0afbcb89f6856a24c0ff4da08097a5
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2020-11-25 13:58:28 +0000

    [core] -Del: do not auto-load all fungw scripting engine plugins - the script plugin will pick the ones needed
    git-svn-id: svn://repo.hu/librnd/trunk@32491 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/hid_init.c
+++ src/librnd/core/hid_init.c
@@ -102,9 +102,6 @@ void rnd_hid_init()
 	rnd_plugin_add_dir(tmp);
 #endif
 
-	/* fungw script pups */
-	rnd_plugin_add_dir(FGW_CFG_PUPDIR);
-
 	/* host app's */
 TODO("make this configurable - add to conf_board_ignores avoid plugin injection")
 	tmp = rnd_concat(rnd_conf.rc.path.exec_prefix, RND_DIR_SEPARATOR_S, "lib", RND_DIR_SEPARATOR_S, "pcb-rnd", RND_DIR_SEPARATOR_S, "plugins", RND_DIR_SEPARATOR_S, HOST, NULL);
