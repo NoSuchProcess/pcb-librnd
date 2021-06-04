df5ea040eb5b04a6ef596ce911bad7fe1147e5ef
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-06-04 12:13:21 +0000

    [lib_wget] -Add: spare fields
    git-svn-id: svn://repo.hu/librnd/trunk@32694 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_wget/lib_wget.h
+++ src/librnd/plugins/lib_wget/lib_wget.h
@@ -30,6 +30,13 @@
 typedef struct {
 	const char **header;
 	const char *post_file;
+
+	/* Spare: see doc/developer/spare.txt */
+	void (*spare_f1)(void), (*spare_f2)(void);
+	long spare_l1, spare_l2, spare_l3, spare_l4;
+	void *spare_p1, *spare_p2, *spare_p3, *spare_p4;
+	double spare_d1, spare_d2, spare_d3, spare_d4;
+	rnd_coord_t spare_c1, spare_c2, spare_c3, spare_c4;
 } rnd_wget_opts_t;
 
 /* download the document at url to a local file ofn; if update is non-zero,
