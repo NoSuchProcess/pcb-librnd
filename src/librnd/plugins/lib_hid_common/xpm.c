089f7f447cade60e0902193a6e7acd375a2b979e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-06-05 05:45:53 +0000

    [lib_hid_common] -Import: online-help xpm from pcb-rnd
    git-svn-id: svn://repo.hu/librnd/trunk@32711 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_hid_common/xpm.c
+++ src/librnd/plugins/lib_hid_common/xpm.c
@@ -5,10 +5,12 @@
 
 #include "xpm/question.xpm"
 #include "xpm/warning.xpm"
+#include "xpm/online_help.xpm"
 
 const char **pcp_dlg_xpm_by_name(const char *name)
 {
 	if (strcmp(name, "question") == 0) return question_xpm;
 	if (strcmp(name, "warning") == 0) return warning_xpm;
+	if (strcmp(name, "online_help") == 0) return online_help_xpm;
 	return NULL;
 }
