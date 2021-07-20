6a8088aeb1b30e5818286877ec01aa3cf82cede2
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-07-20 06:13:46 +0000

    [scconfig] -Update: version to 3.0.1-dev
    git-svn-id: svn://repo.hu/librnd/trunk@32927 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -11,7 +11,7 @@
 #include "util/arg_auto_set.h"
 #include "Rev.h"
 
-#define version "3.0.0"
+#define version "3.0.1-dev"
 
 #define LIBRND_ROOT         "../src/librnd"
 #define LIBRND_PLUGIN_ROOT  "../src/librnd/plugins"
