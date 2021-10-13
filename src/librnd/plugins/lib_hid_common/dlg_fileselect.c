f2773ce3b7df082b86509515b968b465a658ecc2
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-10-13 08:43:26 +0000

    [fsd] -Add: basic dialog layout
    git-svn-id: svn://repo.hu/librnd/trunk@33076 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_hid_common/dlg_fileselect.c
+++ src/librnd/plugins/lib_hid_common/dlg_fileselect.c
@@ -28,11 +28,107 @@
 
 #include "config.h"
 
+#include <genht/hash.h>
+#include <librnd/core/error.h>
+#include <librnd/core/hid_dad.h>
+
 #include "dlg_fileselect.h"
+
+typedef struct{
+	RND_DAD_DECL_NOINIT(dlg)
+	int active;
+	int wpath, wdir[16], wdirlong, wshand, wfilelist;
+	rnd_hid_t *hid;
+} fsd_ctx_t;
+
+static fsd_ctx_t fsd_ctx;
+
+
+TODO("Maybe remove this")
+static void fsd_close_cb(void *caller_data, rnd_hid_attr_ev_t ev)
+{
+#if 0
+	fsd_ctx_t *fsd = caller_data;
+	fsd->active = 0;
+#endif
+}
 
 char *rnd_dlg_fileselect(rnd_hid_t *hid, const char *title, const char *descr, const char *default_file, const char *default_ext, const rnd_hid_fsd_filter_t *flt, const char *history_tag, rnd_hid_fsd_flags_t flags, rnd_hid_dad_subdialog_t *sub)
 {
+	fsd_ctx_t *ctx = &fsd_ctx;
+	rnd_hid_dad_buttons_t clbtn[] = {{"Cancel", 1}, {"ok", 0}, {NULL, 0}};
+	const char *shc_hdr[] = { "Shortcuts", NULL };
+	const char *filelist_hdr[] = { "Name", "Size", "Modified", NULL };
+	int n;
 
+	if (ctx->active) {
+		rnd_message(RND_MSG_ERROR, "Recursive call of rnd_dlg_fileselect\n");
+		return NULL;
+	}
+
+	memset(ctx, 0, sizeof(fsd_ctx_t));
+	ctx->active = 1;
+	ctx->hid = hid;
+
+	RND_DAD_BEGIN_VBOX(ctx->dlg);
+		RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL);
+
+		/* manual edit */
+		RND_DAD_BEGIN_HBOX(ctx->dlg);
+			RND_DAD_STRING(ctx->dlg);
+				RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL);
+				ctx->wpath = RND_DAD_CURRENT(ctx->dlg);
+		RND_DAD_END(ctx->dlg);
+
+		/* directory helper */
+		RND_DAD_BEGIN_VBOX(ctx->dlg);
+			RND_DAD_BEGIN_HBOX(ctx->dlg);
+				RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_FRAME | RND_HATF_SCROLL);
+				for(n = 0; n < 8; n++) {
+					RND_DAD_BUTTON(ctx->dlg, "dir1");
+						ctx->wdir[n] = RND_DAD_CURRENT(ctx->dlg);
+				}
+				RND_DAD_LABEL(ctx->dlg, "...");
+					ctx->wdirlong = RND_DAD_CURRENT(ctx->dlg);
+				for(n = 0; n < 8; n++) {
+					RND_DAD_BUTTON(ctx->dlg, "dir2");
+						ctx->wdir[n+8] = RND_DAD_CURRENT(ctx->dlg);
+				}
+			RND_DAD_END(ctx->dlg);
+		RND_DAD_END(ctx->dlg);
+
+		/* lists */
+		RND_DAD_BEGIN_HPANE(ctx->dlg);
+			RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL);
+			RND_DAD_TREE(ctx->dlg, 1, 0, shc_hdr);
+				RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL | RND_HATF_FRAME);
+				ctx->wshand = RND_DAD_CURRENT(ctx->dlg);
+			RND_DAD_TREE(ctx->dlg, 3, 0, filelist_hdr);
+				RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL | RND_HATF_FRAME);
+				ctx->wfilelist = RND_DAD_CURRENT(ctx->dlg);
+		RND_DAD_END(ctx->dlg);
+
+		/* custom widgets and standard buttons */
+		RND_DAD_BEGIN_HBOX(ctx->dlg);
+
+			TODO("subdialog");
+
+			/* spring */
+			RND_DAD_BEGIN_HBOX(ctx->dlg);
+				RND_DAD_COMPFLAG(ctx->dlg, RND_HATF_EXPFILL);
+			RND_DAD_END(ctx->dlg);
+
+			/* close button */
+			RND_DAD_BUTTON_CLOSES(ctx->dlg, clbtn);
+		RND_DAD_END(ctx->dlg);
+
+	RND_DAD_END(ctx->dlg);
+
+	RND_DAD_DEFSIZE(ctx->dlg, 200, 300);
+	RND_DAD_NEW("file_selection_dialog", ctx->dlg, title, ctx, rnd_true, fsd_close_cb);
+	RND_DAD_RUN(ctx->dlg);
+
+	ctx->active = 0;
 	return NULL;
 }
 
@@ -41,6 +137,19 @@ const char rnd_acts_FsdTest[] = "FsdTest()";
 const char rnd_acth_FsdTest[] = "Central, DAD based File Selection Dialog demo";
 fgw_error_t rnd_act_FsdTest(fgw_arg_t *res, int argc, fgw_arg_t *argv)
 {
+	char *fn;
+	rnd_hid_fsd_filter_t *flt = NULL;
+	rnd_hid_fsd_flags_t flags = RND_HID_FSD_MAY_NOT_EXIST;
+	rnd_hid_dad_subdialog_t *sub = NULL;
+
+	fn = rnd_dlg_fileselect(rnd_gui, "FsdTest", "DAD File Selection Dialog demo", "fsd.txt", ".txt", flt, "fsd_hist", flags, sub);
+
+
+	if (fn != NULL)
+		rnd_message(RND_MSG_INFO, "FSD: fn='%s'\n", fn);
+	else
+		rnd_message(RND_MSG_INFO, "FSD: no file\n");
+
 	return -1;
 }
 
