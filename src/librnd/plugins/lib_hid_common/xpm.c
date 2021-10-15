4ab499877155c841321a72f76a1da4ab3adfc999
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-10-15 13:03:57 +0000

    [lib_hid_common] -Import: plus and minus xpm's from pcb-rnd (layer selector)
    git-svn-id: svn://repo.hu/librnd/trunk@33097 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_hid_common/xpm.c
+++ src/librnd/plugins/lib_hid_common/xpm.c
@@ -8,12 +8,16 @@
 #include "xpm/question.xpm"
 #include "xpm/warning.xpm"
 #include "xpm/online_help.xpm"
+#include "xpm/plus.xpm"
+#include "xpm/minus.xpm"
 
 const char **rnd_dlg_xpm_by_name(const char *name)
 {
 	if (strcmp(name, "question") == 0) return question_xpm;
 	if (strcmp(name, "warning") == 0) return warning_xpm;
 	if (strcmp(name, "online_help") == 0) return online_help_xpm;
+	if (strcmp(name, "plus") == 0) return plus_xpm;
+	if (strcmp(name, "minus") == 0) return minus_xpm;
 	return NULL;
 }
 
