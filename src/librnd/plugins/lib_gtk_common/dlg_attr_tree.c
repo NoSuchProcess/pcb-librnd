730743ab4e8048a769cf258633058e2b7698ed1b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-10-18 06:31:36 +0000

    [lib_gtk_common] -Fix: more robust fix for subtree expand when the arrow is clicked: let gtk handle the arrow
    git-svn-id: svn://repo.hu/librnd/trunk@33145 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk_common/dlg_attr_tree.c
+++ src/librnd/plugins/lib_gtk_common/dlg_attr_tree.c
@@ -323,6 +323,19 @@ static gboolean ghid_tree_table_key_press_cb(GtkTreeView *tree_view, GdkEventKey
 	return TRUE;
 }
 
+/* Return the x coord of the first non-arrow pixel or -1 if there's no arrow */
+static int ghid_tree_table_arrow_width(GtkTreeView *tv, GtkTreePath *tp)
+{
+	GdkRectangle rect;
+	GtkTreeViewColumn *col = gtk_tree_view_get_expander_column(tv);
+
+	if (col == NULL)
+		return -1;
+	gtk_tree_view_get_cell_area(tv, tp, col, &rect);
+
+	return rect.x;
+}
+
 /* Handle the double-click to be equivalent to "row-activated" signal */
 static gboolean ghid_tree_table_button_press_cb(GtkWidget *widget, GdkEvent *ev, rnd_hid_attribute_t *attr)
 {
@@ -335,6 +348,10 @@ static gboolean ghid_tree_table_button_press_cb(GtkWidget *widget, GdkEvent *ev,
 		model = gtk_tree_view_get_model(tv);
 		gtk_tree_view_get_path_at_pos(tv, ev->button.x, ev->button.y, &path, NULL, NULL, NULL);
 		if (path != NULL) {
+			int minx = ghid_tree_table_arrow_width(tv, path);
+			if (ev->button.x < minx)
+				return FALSE; /* do not activate whenclicked on the arrow (interferes with gtk auto expand) */
+
 			gtk_tree_model_get_iter(model, &iter, path);
 			tree_row_activated(tv, path, NULL, attr);
 		}
@@ -353,13 +370,17 @@ static gboolean ghid_tree_table_button_release_cb(GtkWidget *widget, GdkEvent *e
 	model = gtk_tree_view_get_model(tv);
 	gtk_tree_view_get_path_at_pos(tv, ev->button.x, ev->button.y, &path, NULL, NULL, NULL);
 	if (path != NULL) {
+		int minx = ghid_tree_table_arrow_width(tv, path);
+		if (ev->button.x < minx)
+			return FALSE; /* do not activate whenclicked on the arrow (interferes with gtk auto expand) */
+
 		gtk_tree_model_get_iter(model, &iter, path);
 		/* Do not activate the row if LEFT-click on a "parent category" row. */
 		if (ev->button.button != 1 || !gtk_tree_model_iter_has_child(model, &iter))
 			tree_row_activated(tv, path, NULL, attr);
 	}
 
-	return TRUE;
+	return FALSE;
 }
 
 static int ghid_tree_table_set(attr_dlg_t *ctx, int idx, const rnd_hid_attr_val_t *val)
