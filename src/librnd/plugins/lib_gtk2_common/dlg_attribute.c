44db22323431fac4fb5a3a6bd523986744bb0d4b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-11-13 07:37:24 +0000

    [gtk] -Split: move label wrapper for click bind to the compat layer, it works differently in gtk4
    git-svn-id: svn://repo.hu/librnd/trunk@33323 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk2_common/dlg_attribute.c
+++ src/librnd/plugins/lib_gtk2_common/dlg_attribute.c
@@ -1,3 +1,13 @@
 #include "compat.h"
 
+/* Wrap w so that clicks on it are triggering a callback */
+static GtkWidget *wrap_bind_click(GtkWidget *w, GCallback cb, void *cb_data)
+{
+	GtkWidget *event_box = gtk_event_box_new();
+	gtk_container_add(GTK_CONTAINER(event_box), w);
+	g_signal_connect(event_box, "button-press-event", G_CALLBACK(cb), cb_data);
+
+	return event_box;
+}
+
 #include <librnd/plugins/lib_gtk_common/dlg_attribute.c>
