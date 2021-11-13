cbabc8a63dbb44df9ba335eae169c3e90a9f954a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-11-13 04:31:15 +0000

    [gtk] -Cleanup: move back low level top window drawing signal connect/disconnect (mouse and kbd) from gtk2 to common gtk: all calls go through the compat layer
    git-svn-id: svn://repo.hu/librnd/trunk@33318 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk_common/glue_common.c
+++ src/librnd/plugins/lib_gtk_common/glue_common.c
@@ -216,6 +216,42 @@ void rnd_gtk_mode_cursor_main(void)
 	rnd_gtk_mode_cursor(ghidgui);
 }
 
+/* low level keyboard and mouse signal connect/disconnect from top window drawing area */
+static void kbd_input_signals_connect(int idx, void *obj)
+{
+	ghidgui->key_press_handler[idx] = g_signal_connect(G_OBJECT(obj), "key_press_event", G_CALLBACK(rnd_gtk_key_press_cb), ghidgui);
+	ghidgui->key_release_handler[idx] = g_signal_connect(G_OBJECT(obj), "key_release_event", G_CALLBACK(rnd_gtk_key_release_cb), &ghidgui->topwin);
+}
+
+static void kbd_input_signals_disconnect(int idx, void *obj)
+{
+	if (ghidgui->key_press_handler[idx] != 0) {
+		g_signal_handler_disconnect(G_OBJECT(obj), ghidgui->key_press_handler[idx]);
+		ghidgui->key_press_handler[idx] = 0;
+	}
+	if (ghidgui->key_release_handler[idx] != 0) {
+		g_signal_handler_disconnect(G_OBJECT(obj), ghidgui->key_release_handler[idx]);
+		ghidgui->key_release_handler[idx] = 0;
+	}
+}
+
+static void mouse_input_singals_connect(void *obj)
+{
+	ghidgui->button_press_handler = gtkc_bind_mouse_press(obj, rnd_gtkc_xy_ev(&ghidgui->mpress_rs, rnd_gtk_button_press_cb, ghidgui));
+	ghidgui->button_release_handler = gtkc_bind_mouse_release(obj, rnd_gtkc_xy_ev(&ghidgui->mrelease_rs, rnd_gtk_button_release_cb, ghidgui));
+}
+
+static void mouse_input_singals_disconnect(void *obj)
+{
+	if (ghidgui->button_press_handler != 0)
+		g_signal_handler_disconnect(G_OBJECT(ghidgui->port.drawing_area), ghidgui->button_press_handler);
+
+	if (ghidgui->button_release_handler != 0)
+		g_signal_handler_disconnect(ghidgui->port.drawing_area, ghidgui->button_release_handler);
+
+	ghidgui->button_press_handler = ghidgui->button_release_handler = 0;
+}
+
 /* Connect and disconnect just the signals a g_main_loop() will need.
    Cursor and motion events still need to be handled by the top level
    loop, so don't connect/reconnect these.
