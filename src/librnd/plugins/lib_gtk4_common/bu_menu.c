02bce5e1fee1919d32f73a3a575c3536c28f9f52
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-11-15 13:23:56 +0000

    [gtk4] -Add: dummy bu_menu
    git-svn-id: svn://repo.hu/librnd/trunk@33366 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk4_common/bu_menu.c
+++ src/librnd/plugins/lib_gtk4_common/bu_menu.c
@@ -1,2 +1,51 @@
-#include "compat.h"
-#include <librnd/plugins/lib_gtk_common/bu_menu.c>
+/*
+ *                            COPYRIGHT
+ *
+ *  pcb-rnd, interactive printed circuit board design
+ *  Copyright (C) 2021 Tibor Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/pcb-rnd
+ *    lead developer: http://repo.hu/projects/pcb-rnd/contact.html
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ */
+
+
+void rnd_gtk_main_menu_update_toggle_state(rnd_hidlib_t *hidlib, GtkWidget *menubar)
+{
+
+}
+
+int rnd_gtk_create_menu_widget(void *ctx_, int is_popup, const char *name, int is_main, lht_node_t *parent, lht_node_t *ins_after, lht_node_t *menu_item)
+{
+	return -1;
+}
+
+int rnd_gtk_remove_menu_widget(void *ctx, lht_node_t *nd)
+{
+	return -1;
+}
+
+GtkWidget *rnd_gtk_load_menus(rnd_gtk_menu_ctx_t *menu, rnd_hidlib_t *hidlib)
+{
+	return NULL;
+}
+
+GtkWidget *rnd_gtk_menu_widget(lht_node_t *node)
+{
+	return NULL;
+}
