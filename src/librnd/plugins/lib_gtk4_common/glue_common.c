3af17a38e9b1045ae38e71f395d45e198323c494
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-11-19 09:03:29 +0000

    [gtk4] -Add: port adjustment setup to use setter calls instead of direct field manipulation
    git-svn-id: svn://repo.hu/librnd/trunk@33483 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk4_common/glue_common.c
+++ src/librnd/plugins/lib_gtk4_common/glue_common.c
@@ -4,12 +4,17 @@
 /* Update adj limits to match the current zoom level */
 static inline void rnd_gtkc_zoom_adjustment(GtkAdjustment *adj, rnd_coord_t view_size, rnd_coord_t board_size)
 {
-	adj->page_size = MIN(view_size, board_size);
-	adj->lower = -view_size;
-	adj->upper = board_size + adj->page_size;
-	adj->step_increment = adj->page_size / 100.0;
-	adj->page_increment = adj->page_size / 10.0;
-	gtk_signal_emit_by_name (GTK_OBJECT(adj), "changed");
+	double ps = MIN(view_size, board_size);
+
+	gtk_adjustment_set_page_size(adj, ps);
+	gtk_adjustment_set_lower(adj, -view_size);
+	gtk_adjustment_set_upper(adj, board_size + adj->page_size);
+
+	gtk_adjustment_set_step_increment(adj, ps / 100.0);
+	gtk_adjustment_set_page_increment(adj, ps / 10.0);
+
+TODO("This is probably not needed as all fields got written via calls");
+/*	gtk_signal_emit_by_name (GTK_OBJECT(adj), "changed");*/
 }
 
 #include <librnd/plugins/lib_gtk_common/glue_common.c>
