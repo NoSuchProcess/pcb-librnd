fb30d60a851078d1a0e164d4b96414c017a85104
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-11-26 03:02:38 +0000

    [gtk4] -Add: create main menu buttons
    git-svn-id: svn://repo.hu/librnd/trunk@33588 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk4_common/bu_menu.c
+++ src/librnd/plugins/lib_gtk4_common/bu_menu.c
@@ -26,6 +26,9 @@
 
 #include <librnd/core/hidlib.h>
 
+#include "compat.h"
+#include <librnd/plugins/lib_gtk_common/rnd_gtk.h>
+
 #include "bu_menu.h"
 
 void rnd_gtk_main_menu_update_toggle_state(rnd_hidlib_t *hidlib, GtkWidget *menubar)
@@ -43,9 +46,55 @@ int rnd_gtk_remove_menu_widget(void *ctx, lht_node_t *nd)
 	return -1;
 }
 
+static void rnd_gtk_main_menu_add_node(rnd_gtk_menu_ctx_t *ctx, GtkWidget *menu_bar, const lht_node_t *base)
+{
+	lht_node_t *n;
+
+	if (base->type != LHT_LIST) {
+		rnd_hid_cfg_error(base, "Menu description shall be a list (li)\n");
+		abort();
+	}
+
+	/* create main menu buttons */
+	for(n = base->data.list.first; n != NULL; n = n->next) {
+		GtkWidget *btn = gtk_button_new_with_label(n->name);
+		gtkc_box_pack_append(menu_bar, btn, FALSE, 0);
+	}
+}
+
+
 GtkWidget *rnd_gtk_load_menus(rnd_gtk_menu_ctx_t *menu, rnd_hidlib_t *hidlib)
 {
-	return gtk_label_new("implement bu_menu.c");
+	const lht_node_t *mr;
+	GtkWidget *menu_bar = NULL;
+
+	menu->hidlib = hidlib;
+
+	rnd_hid_menu_gui_ready_to_create(rnd_gui);
+
+	mr = rnd_hid_cfg_get_menu(rnd_gui->menu, "/main_menu");
+	if (mr != NULL) {
+		menu_bar = gtkc_hbox_new(0, 0);
+		rnd_gtk_main_menu_add_node(menu, menu_bar, mr);
+		mr->doc->root->user_data = menu;
+		gtk_widget_show(menu_bar);
+	}
+
+	mr = rnd_hid_cfg_get_menu(rnd_gui->menu, "/popups");
+	if (mr != NULL) {
+		/* no need to create menus in advance */
+		if (mr->type != LHT_LIST)
+			rnd_hid_cfg_error(mr, "/popups should be a list\n");
+		mr->doc->root->user_data = menu;
+	}
+
+	mr = rnd_hid_cfg_get_menu(rnd_gui->menu, "/mouse");
+	if (rnd_hid_cfg_mouse_init(rnd_gui->menu, &rnd_gtk_mouse) != 0)
+		rnd_message(RND_MSG_ERROR, "Error: failed to load mouse actions from the hid config lihata - mouse input will not work.");
+
+	rnd_hid_menu_gui_ready_to_modify(rnd_gui);
+
+	return menu_bar;
 }
 
 GtkWidget *rnd_gtk_menu_widget(lht_node_t *node)
