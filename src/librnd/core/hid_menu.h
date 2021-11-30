e8cecfe828211c658105bf80594c7ed2bbe1896b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-11-30 04:15:30 +0000

    [core] -Add: mark some menu item fields obsolete and scheduled removal
    git-svn-id: svn://repo.hu/librnd/trunk@33639 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/hid_menu.h
+++ src/librnd/core/hid_menu.h
@@ -69,11 +69,11 @@ typedef enum {
 	RND_MF_UPDATE_ON,
 	RND_MF_SENSITIVE,
 	RND_MF_TIP,
-	RND_MF_ACTIVE,
+	RND_MF_ACTIVE,         /* obsolete, do not use (will be removed in 4.0.0) */
 	RND_MF_ACTION,
-	RND_MF_FOREGROUND,
-	RND_MF_BACKGROUND,
-	RND_MF_FONT
+	RND_MF_FOREGROUND,     /* obsolete, do not use (will be removed in 4.0.0) */
+	RND_MF_BACKGROUND,     /* obsolete, do not use (will be removed in 4.0.0) */
+	RND_MF_FONT            /* obsolete, do not use (will be removed in 4.0.0) */
 } rnd_hid_cfg_menufield_t;
 
 /* Return a field of a submenu and optionally fill in field_name with the
