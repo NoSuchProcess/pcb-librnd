f077128f3190e3f935b6c4757f24e20f124dd10c
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2021-12-21 06:42:43 +0000

    [lib_hid_gl] -Add: describe the function of backend config settings
    git-svn-id: svn://repo.hu/librnd/trunk@33877 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_hid_gl/lib_hid_gl_conf.h
+++ src/librnd/plugins/lib_hid_gl/lib_hid_gl_conf.h
@@ -7,9 +7,9 @@ typedef struct {
 	const struct {
 		const struct {
 			const struct backend {
-				RND_CFT_BOOLEAN disable_direct;
-				RND_CFT_BOOLEAN disable_vao;
-				RND_CFT_LIST preference;
+				RND_CFT_BOOLEAN disable_direct;    /* do not use the opengl direct draw backend (client state imlementation); needs restart to take effect */
+				RND_CFT_BOOLEAN disable_vao;       /* do not use the opengl vao draw backend (vertex array object imlementation); needs restart to take effect */
+				RND_CFT_LIST preference;           /* ordered list of opengl draw backends; first one that initializes without error will be chosen for rendering; needs restart to take effect */
 			} backend;
 		} lib_hid_gl;
 	} plugins;
