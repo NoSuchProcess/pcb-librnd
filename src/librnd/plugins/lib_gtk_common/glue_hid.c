e6ab1b8e69ffa1a1b0491c0faab593454a9b8e3d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-02-20 09:32:48 +0000

    [gtk] -Fix: don't call implementation-specific pixmap uninit if it is not available
    git-svn-id: svn://repo.hu/librnd/trunk@34024 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk_common/glue_hid.c
+++ src/librnd/plugins/lib_gtk_common/glue_hid.c
@@ -570,7 +570,8 @@ static void rnd_gtkg_init_pixmap(rnd_hid_t *hid, rnd_pixmap_t *pxm)
 static void rnd_gtkg_uninit_pixmap_(rnd_hid_t *hid, rnd_pixmap_t *pxm)
 {
 	rnd_gtk_t *gctx = hid->hid_data;
-	gctx->impl.uninit_pixmap(gctx->hidlib, (rnd_gtk_pixmap_t *)(pxm->hid_data));
+	if ((gctx != NULL) && (gctx->impl.uninit_pixmap != NULL))
+		gctx->impl.uninit_pixmap(gctx->hidlib, (rnd_gtk_pixmap_t *)(pxm->hid_data));
 	rnd_gtkg_uninit_pixmap_low((rnd_gtk_pixmap_t *)(pxm->hid_data));
 	free(pxm->hid_data);
 }
