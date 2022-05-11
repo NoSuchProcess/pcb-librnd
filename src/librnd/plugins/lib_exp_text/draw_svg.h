f01ee9ea770c2461ee0ca8324f28d39260bcd51a
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-05-11 14:11:57 +0000

    [lib_exp_text] -Add: spare fields in public structs
    git-svn-id: svn://repo.hu/librnd/trunk@34124 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_exp_text/draw_svg.h
+++ src/librnd/plugins/lib_exp_text/draw_svg.h
@@ -18,6 +18,12 @@ typedef struct {
 
 	/* private: pcb-rnd leftover */
 	int photo_mode, photo_noise, drawing_mask, drawing_hole;
+
+	/* Spare: see doc/developer/spare.txt */
+	void (*spare_f1)(void), (*spare_f2)(void);
+	long spare_l1, spare_l2, spare_l3, spare_l4;
+	void *spare_p1, *spare_p2, *spare_p3, *spare_p4;
+	double spare_d1, spare_d2, spare_d3, spare_d4;
 } rnd_svg_t;
 
 
--- src/librnd/plugins/lib_exp_text/media.h
+++ src/librnd/plugins/lib_exp_text/media.h
@@ -2,6 +2,13 @@ typedef struct {
 	const char *name;
 	rnd_coord_t width, height; /* orientation: landscape */
 	rnd_coord_t margin_x, margin_y;
+
+	/* Spare: see doc/developer/spare.txt */
+	void (*spare_f1)(void), (*spare_f2)(void);
+	long spare_l1, spare_l2;
+	void *spare_p1, *spare_p2;
+	rnd_coord_t spare_c1, spare_c2;
+	double spare_d1, spare_d2;
 } rnd_media_t;
 
 extern rnd_media_t rnd_media_data[];
