514b55c2bc044349fac466b882b6e98683e53a67
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-05-13 16:25:43 +0000

    [scconfig] -Fix: common args, like --debug or --symbols should use app's defined APP name instead of hardwired librnd
    git-svn-id: svn://repo.hu/librnd/trunk@34170 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/scconfig/hooks_common.h
+++ src/librnd/scconfig/hooks_common.h
@@ -1,3 +1,11 @@
+#ifndef LIBRND_SCCONFIG_APP_TREE
+/* for compatibility */
+/* TODO:pcb1 - make this an #error in librnd4 */
+#define APP "pcb"
+#else
+#define APP LIBRND_SCCONFIG_APP_TREE
+#endif
+
 /* the caller needs to provide these: */
 static void help1(void);
 const arg_auto_set_t disable_libs[];
@@ -65,18 +73,18 @@ static int rnd_hook_custom_arg_(const char *key, const char *value, const arg_au
 	}
 	if (strcmp(key, "debug") == 0) {
 		put("/local/pcb/debug", strue);
-		put("/local/librnd/debug", strue);
+		put("/local/" APP"/debug", strue);
 		pup_set_debug(strue);
 		return 1;
 	}
 	if (strcmp(key, "profile") == 0) {
 		put("/local/pcb/profile", strue);
-		put("/local/librnd/profile", strue);
+		put("/local/" APP "/profile", strue);
 		return 1;
 	}
 	if (strcmp(key, "symbols") == 0) {
 		put("/local/pcb/symbols", strue);
-		put("/local/librnd/symbols", strue);
+		put("/local/" APP "/symbols", strue);
 		return 1;
 	}
 	if ((strcmp(key, "all") == 0) || (strcmp(key, "force-all") == 0)) {
@@ -306,15 +314,15 @@ void rnd_hook_postinit()
 	put("/local/confdir", "");
 #endif
 	put("/local/pcb/debug", sfalse);
-	put("/local/librnd/debug", sfalse);
+	put("/local/" APP "/debug", sfalse);
 	put("/local/pcb/profile", sfalse);
-	put("/local/librnd/profile", sfalse);
+	put("/local/" APP "/profile", sfalse);
 	put("/local/pcb/symbols", sfalse);
-	put("/local/librnd/symbols", sfalse);
+	put("/local/" APP "/symbols", sfalse);
 	put("/local/pcb/disable_so", sfalse);
-	put("/local/librnd/disable_so", sfalse);
+	put("/local/" APP "/disable_so", sfalse);
 	put("/local/pcb/want_static_librnd", sfalse);
-	put("/local/librnd/want_static_librnd", sfalse);
+	put("/local/" APP "/want_static_librnd", sfalse);
 
 #undef plugin_def
 #undef plugin_header
@@ -448,3 +456,4 @@ static void print_sum_cfg_val(const char *node, const char *desc)
 	printf("%-55s %s\n", desc, state);
 }
 
+#undef APP
