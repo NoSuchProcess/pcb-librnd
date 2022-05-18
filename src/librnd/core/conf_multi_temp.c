bc7a5dc84672fda718cd92bbe06d3173686e2430
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-05-18 10:12:33 +0000

    [conf] -Add: alloc/free/load/save config states (temporary multi-sheet support)
    git-svn-id: svn://repo.hu/librnd/trunk@34215 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/conf_multi_temp.c
+++ src/librnd/core/conf_multi_temp.c
@@ -28,7 +28,7 @@
 
 #include "conf_multi_temp.h"
 
-typedef struct rnd_conf_state_s {
+struct rnd_conf_state_s {
 	htsi_t conf_interns;
 	int conf_files_inited;
 	int rnd_conf_in_production;
@@ -40,5 +40,68 @@ typedef struct rnd_conf_state_s {
 	htsp_t *rnd_conf_fields;
 	vmst_t merge_subtree;
 	int rnd_conf_rev;
-} rnd_conf_state_t;
 
+	int valid;
+};
+
+rnd_conf_state_t *rnd_conf_state_alloc(void)
+{
+	return calloc(sizeof(rnd_conf_state_t), 1);
+}
+
+void rnd_conf_state_free(rnd_conf_state_t *cs)
+{
+	TODO("free all fields");
+	free(cs);
+}
+
+
+#define SAVE(field) \
+	memcpy(&dst->field, &field, sizeof(field)); \
+	memset(&field, 0, sizeof(field));
+
+void rnd_conf_state_save(rnd_conf_state_t *dst)
+{
+	assert(!dst->valid);
+
+	SAVE(conf_interns);
+	SAVE(conf_files_inited);
+	SAVE(rnd_conf_in_production);
+	SAVE(rnd_conf_main_root);
+	SAVE(rnd_conf_main_root_replace_cnt);
+	SAVE(rnd_conf_main_root_lock);
+	SAVE(rnd_conf_lht_dirty);
+	SAVE(rnd_conf_plug_root);
+	SAVE(rnd_conf_fields);
+	SAVE(merge_subtree);
+	SAVE(rnd_conf_rev);
+
+	dst->valid = 1;
+}
+
+#undef SAVE
+
+#define LOAD(field) \
+	memcpy(&field, &src->field, sizeof(field)); \
+	memset(&src->field, 0, sizeof(field));
+
+void rnd_conf_state_load(rnd_conf_state_t *src)
+{
+	assert(src->valid);
+
+	LOAD(conf_interns);
+	LOAD(conf_files_inited);
+	LOAD(rnd_conf_in_production);
+	LOAD(rnd_conf_main_root);
+	LOAD(rnd_conf_main_root_replace_cnt);
+	LOAD(rnd_conf_main_root_lock);
+	LOAD(rnd_conf_lht_dirty);
+	LOAD(rnd_conf_plug_root);
+	LOAD(rnd_conf_fields);
+	LOAD(merge_subtree);
+	LOAD(rnd_conf_rev);
+
+	src->valid = 0;
+}
+
+#undef LOAD
--- src/librnd/core/conf_multi_temp.h
+++ src/librnd/core/conf_multi_temp.h
@@ -3,3 +3,20 @@
    DO NOT USE THIS API outside of sch-rnd.
 
 */
+
+typedef struct rnd_conf_state_s rnd_conf_state_t;
+
+
+/* Allocate storage where the global config can be saved to */
+rnd_conf_state_t *rnd_conf_state_alloc(void);
+
+/* Free storage and all fields of a conf state */
+void rnd_conf_state_free(rnd_conf_state_t *cs);
+
+/* Save global config to dst then zero out global config; dst becomes valid
+   so it can be loaded later. */
+void rnd_conf_state_save(rnd_conf_state_t *dst);
+
+/* Load global config from src; zero out fields of src and mark it invalid
+   so it can not be loaded twice. */
+void rnd_conf_state_load(rnd_conf_state_t *src);
