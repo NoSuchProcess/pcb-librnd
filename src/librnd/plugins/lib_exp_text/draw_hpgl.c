3ea91a7b81d46d66baf3dfe607258e5905e8d551
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-06-09 13:21:21 +0000

    [lib_exp_text] -Add: hpgl low level: endpoint hash table type
    git-svn-id: svn://repo.hu/librnd/trunk@34279 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_exp_text/draw_hpgl.c
+++ src/librnd/plugins/lib_exp_text/draw_hpgl.c
@@ -10,8 +10,8 @@ typedef enum {
 } path_obj_type_t;
 
 typedef struct {
+	rnd_coord_t x, y; /* end coords; must be the first field for the hash */
 	path_obj_type_t type;
-	rnd_coord_t x, y; /* end coords */
 	union {
 		struct {
 			rnd_coord_t r, cx, cy; /* radius and center */
@@ -34,3 +34,12 @@ typedef struct {
 #include <genvector/genvector_impl.c>
 #include <genvector/genvector_undef.h>
 
+/* endpoint coord[2] -> vtph* hash */
+#define HT_HAS_CONST_KEY
+typedef rnd_coord_t *htph_key_t;                /* array of [2] for x;y */
+typedef const rnd_coord_t *htph_const_key_t;    /* array of [2] for x;y */
+typedef vtph_t *htph_value_t;
+#define HT(x) htph_ ## x
+#include <genht/ht.h>
+#include <genht/ht.c>
+#undef HT
