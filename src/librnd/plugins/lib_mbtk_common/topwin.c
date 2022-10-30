36caf38e7f8515ee58bced088caece6f46910321
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-10-30 08:02:47 +0000

    [mbtk] -Split: topwin struct into a reusable .h, the menu system is going to depend on it
    git-svn-id: svn://repo.hu/librnd/trunk@34480 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_mbtk_common/topwin.c
+++ src/librnd/plugins/lib_mbtk_common/topwin.c
@@ -1,28 +1,4 @@
-struct rnd_mbtk_topwin_s {
-	mbtk_window_t *win;
-
-	unsigned active:1;
-
-	/*** widgets (local allocation) ***/
-
-	/* top hbox with menu, toolbar and readouts */
-	mbtk_box_t vbox_main, top_spring;
-	mbtk_box_t top_bar_background, top_hbox, menu_hbox, menubar_toolbar_vbox;
-	mbtk_menu_bar_t menu_bar;
-	mbtk_box_t position_hbox;
-
-	mbtk_pane_t hpaned_middle;
-	mbtk_box_t left_toolbar, vbox_middle, info_hbox, drw_hbox1, drw_hbox2;
-	mbtk_canvas_native_t drawing_area;
-	mbtk_scrollbar_t vscroll, hscroll;
-
-	mbtk_box_t bottom_hbox;
-	mbtk_label_t cmd_prompt, cmd;
-
-	/* docking */
-	mbtk_box_t dockbox[RND_HID_DOCK_max];
-	gdl_list_t dock[RND_HID_DOCK_max];
-};
+#include "topwin.h"
 
 static void rnd_mbtk_topwin_alloc(rnd_mbtk_t *mctx)
 {
@@ -164,6 +140,9 @@ static void rnd_mbtk_populate_topwin(rnd_mbtk_t *mctx)
 
 	mbtk_widget_hide(&tw->cmd_prompt.w, 1);
 	mbtk_widget_hide(&tw->cmd.w, 1);
+
+	rnd_mbtk_load_menus(mctx);
+
 
 	TODO("implement these:");
 /*
---
+++ src/librnd/plugins/lib_mbtk_common/topwin.h
@@ -0,0 +1,32 @@
+#ifndef LIBRND_LIB_MBTK_COMMON_TOPWIN
+#define LIBRND_LIB_MBTK_COMMON_TOPWIN
+
+#include <libmbtk/widget.h>
+
+struct rnd_mbtk_topwin_s {
+	mbtk_window_t *win;
+
+	unsigned active:1;
+
+	/*** widgets (local allocation) ***/
+
+	/* top hbox with menu, toolbar and readouts */
+	mbtk_box_t vbox_main, top_spring;
+	mbtk_box_t top_bar_background, top_hbox, menu_hbox, menubar_toolbar_vbox;
+	mbtk_menu_bar_t menu_bar;
+	mbtk_box_t position_hbox;
+
+	mbtk_pane_t hpaned_middle;
+	mbtk_box_t left_toolbar, vbox_middle, info_hbox, drw_hbox1, drw_hbox2;
+	mbtk_canvas_native_t drawing_area;
+	mbtk_scrollbar_t vscroll, hscroll;
+
+	mbtk_box_t bottom_hbox;
+	mbtk_label_t cmd_prompt, cmd;
+
+	/* docking */
+	mbtk_box_t dockbox[RND_HID_DOCK_max];
+	gdl_list_t dock[RND_HID_DOCK_max];
+};
+
+#endif
