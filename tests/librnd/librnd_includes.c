40f61957983a91543741ace15651c37206582631
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-18 10:24:17 +0000

    [tests] -Add: include fontlib in the testing
    git-svn-id: svn://repo.hu/librnd/trunk@34578 1d61de99-560f-4dba-8957-1e505d42358b

--- tests/librnd/librnd_includes.c
+++ tests/librnd/librnd_includes.c
@@ -1,5 +1,16 @@
 #define RND_APP_PREFIX(x)  rndtest_ ## x
 
+/* font */
+#include <librnd/core/global_typedefs.h>
+#include <librnd/src_3rd/liblihata/lihata.h>
+#include <librnd/src_3rd/liblihata/dom.h>
+
+static int PARSE_COORD(rnd_coord_t *dst, lht_node_t *src) { return 0; }
+static int PARSE_DOUBLE(double *dst, lht_node_t *src) { return 0; }
+static lht_node_t *HASH_GET(lht_node_t *hash, const char *name) { return NULL; }
+static int RND_LHT_ERROR(lht_node_t *nd, char *fmt, ...) { return 0; }
+
+
 /* header integrity test: nothing should be included from src/ that is
    not part of librnd */
 #include "inc_all.h"
