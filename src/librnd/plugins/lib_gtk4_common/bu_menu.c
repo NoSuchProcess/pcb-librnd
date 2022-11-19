54491d267946bae9f121bc89b52a90aa7f862e51
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-19 12:13:15 +0000

    [gkt4] -Fix: missing #include after the API renames
    git-svn-id: svn://repo.hu/librnd/trunk@34624 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_gtk4_common/bu_menu.c
+++ src/librnd/plugins/lib_gtk4_common/bu_menu.c
@@ -29,6 +29,7 @@
 
 #include "compat.h"
 #include <librnd/core/hid_cfg_action.h>
+#include <librnd/hid/hid_menu.h>
 #include <librnd/plugins/lib_gtk_common/rnd_gtk.h>
 #include <librnd/plugins/lib_gtk_common/in_keyboard.h>
 #include <librnd/plugins/lib_hid_common/menu_helper.h>
