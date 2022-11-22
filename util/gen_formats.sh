f93babdeabaebd9b3125846f9fc1da623b79f688
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-22 08:04:04 +0000

    [util] -Add: missing copyright header in gen_formats; explain the API
    git-svn-id: svn://repo.hu/librnd/trunk@34677 1d61de99-560f-4dba-8957-1e505d42358b

--- util/gen_formats.sh
+++ util/gen_formats.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
-PLUGINS=../../../../src_plugins
+#   gen_formats - generate file format listing for the doc
+#   Copyright (C) 2017 Tibor 'Igor2' Palinkas
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#   http://repo.hu/projects/librnd
+
+#configuration:
+# $APP      name of the application
+# $PLUGINS  relative path to plugins, e.g. ../../../../src_plugins
 
 echo '
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
