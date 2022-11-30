84181b447894a2bd871338100dc40dbd8996e35d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-11-30 13:10:28 +0000

    [hid] -Fix: call HID's crosshair notification only if the current render HID wants that (avoids calling crosshair functions while export)
    git-svn-id: svn://repo.hu/librnd/trunk@34756 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/hid/hid.c
+++ src/librnd/hid/hid.c
@@ -35,8 +35,8 @@
 
 void rnd_hid_notify_crosshair_change(rnd_design_t *hl, rnd_bool changes_complete)
 {
-	if (rnd_gui->notify_crosshair_change)
-		rnd_gui->notify_crosshair_change(rnd_gui, changes_complete);
+	if (rnd_render->notify_crosshair_change)
+		rnd_render->notify_crosshair_change(rnd_gui, changes_complete);
 	rnd_event(hl, RND_EVENT_CROSSHAIR_MOVE, "i", (int)changes_complete, NULL);
 }
 
