06b7b999129dbc5641e2fa29168c27f18beba990
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-12-01 06:10:45 +0000

    [lib_hid_common] -Add: publish rnd_dlg_export() so non-standard export dialogs can be implemented (e.g. project export in sch-rnd)
    git-svn-id: svn://repo.hu/librnd/trunk@34761 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_hid_common/dlg_export.h
+++ src/librnd/plugins/lib_hid_common/dlg_export.h
@@ -5,3 +5,8 @@ fgw_error_t rnd_act_ExportDialog(fgw_arg_t *ores, int oargc, fgw_arg_t *oargv);
 extern const char rnd_acts_PrintDialog[];
 extern const char rnd_acth_PrintDialog[];
 fgw_error_t rnd_act_PrintDialog(fgw_arg_t *ores, int oargc, fgw_arg_t *oargv);
+
+/* Creates and runs the non-modal dialog. Useful for implementing alternative
+   actioins, e.g. for exporting a whole project */
+void rnd_dlg_export(const char *title, int exporters, int printers, rnd_design_t *dsg, void *appspec);
+
