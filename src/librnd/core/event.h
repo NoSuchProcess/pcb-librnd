969a668c9ee0d60f6e1249a4c4147d180a9d2c87
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-12-04 10:06:28 +0000

    [core] -Add: mark each event as [a] or [d] (for per-app or per-design)
    git-svn-id: svn://repo.hu/librnd/trunk@34787 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/event.h
+++ src/librnd/core/event.h
@@ -31,55 +31,59 @@
 #include <librnd/core/global_typedefs.h>
 #include <librnd/core/hidlib.h>
 
+/* Prefix:
+   [d]: per-design: generated for a specific rnd_design_t
+   [a]: per-app: generated once, not targeted or specific for a rnd_design_t
+*/
 typedef enum {
-	RND_EVENT_GUI_INIT,               /* finished initializing the GUI called right before the main loop of the GUI; args: (void) */
-	RND_EVENT_CLI_ENTER,              /* the user pressed enter on a CLI command - called before parsing the line for actions; args: (str commandline) */
+	RND_EVENT_GUI_INIT,               /* [a] finished initializing the GUI called right before the main loop of the GUI; args: (void) */
+	RND_EVENT_CLI_ENTER,              /* [a] the user pressed enter on a CLI command - called before parsing the line for actions; args: (str commandline) */
 
-	RND_EVENT_TOOL_REG,               /* called after a new tool has been registered; arg is (rnd_tool_t *) of the new tool */
-	RND_EVENT_TOOL_SELECT_PRE,        /* called before a tool is selected; arg is (int *ok, int toolid); if ok is non-zero if the selection is accepted */
-	RND_EVENT_TOOL_RELEASE,           /* no arg */
-	RND_EVENT_TOOL_PRESS,             /* no arg */
+	RND_EVENT_TOOL_REG,               /* [a] called after a new tool has been registered; arg is (rnd_tool_t *) of the new tool */
+	RND_EVENT_TOOL_SELECT_PRE,        /* [a] called before a tool is selected; arg is (int *ok, int toolid); if ok is non-zero if the selection is accepted */
+	RND_EVENT_TOOL_RELEASE,           /* [a] no arg */
+	RND_EVENT_TOOL_PRESS,             /* [a] no arg */
 
-	RND_EVENT_BUSY,                   /* called before/after CPU-intensive task begins; argument is an integer: 1 is before, 0 is after */
-	RND_EVENT_LOG_APPEND,             /* called after a new log line is appended; arg is a pointer to the log line */
-	RND_EVENT_LOG_CLEAR,              /* called after a clear; args: two pointers; unsigned long "from" and "to" */
+	RND_EVENT_BUSY,                   /* [a] called before/after CPU-intensive task begins; argument is an integer: 1 is before, 0 is after */
+	RND_EVENT_LOG_APPEND,             /* [a] called after a new log line is appended; arg is a pointer to the log line */
+	RND_EVENT_LOG_CLEAR,              /* [a] called after a clear; args: two pointers; unsigned long "from" and "to" */
 
-	RND_EVENT_GUI_LEAD_USER,          /* GUI aid to lead the user attention to a specific location on the design in the main drawing area; args: (coord x, coord y, int enabled) */
-	RND_EVENT_GUI_DRAW_OVERLAY_XOR,   /* called in design draw after finished drawing the xor marks, still in xor draw mode; argument is a pointer to the GC to use for drawing */
-	RND_EVENT_USER_INPUT_POST,        /* generated any time any user input reaches core, after processing it */
-	RND_EVENT_USER_INPUT_KEY,         /* generated any time a keypress is registered by the hid_cfg_key code (may be one key stroke of a multi-stroke sequence) */
+	RND_EVENT_GUI_LEAD_USER,          /* [a] GUI aid to lead the user attention to a specific location on the design in the main drawing area; args: (coord x, coord y, int enabled) */
+	RND_EVENT_GUI_DRAW_OVERLAY_XOR,   /* [a] called in design draw after finished drawing the xor marks, still in xor draw mode; argument is a pointer to the GC to use for drawing */
+	RND_EVENT_USER_INPUT_POST,        /* [a] generated any time any user input reaches core, after processing it */
+	RND_EVENT_USER_INPUT_KEY,         /* [a] generated any time a keypress is registered by the hid_cfg_key code (may be one key stroke of a multi-stroke sequence) */
 
-	RND_EVENT_CROSSHAIR_MOVE,         /* called when the crosshair changed coord; arg is an integer which is zero if more to come */
+	RND_EVENT_CROSSHAIR_MOVE,         /* [a] called when the crosshair changed coord; arg is an integer which is zero if more to come */
 
-	RND_EVENT_DAD_NEW_DIALOG,         /* called by the GUI after a new DAD dialog is open; args are pointer hid_ctx,  string dialog id and a pointer to int[4] for getting back preferre {x,y,w,h} (-1 means unknown) */
-	RND_EVENT_DAD_NEW_GEO,            /* called by the GUI after the window geometry _may_ have changed; args are: void *hid_ctx, const char *dialog id, int x1, int y1, int width, int height */
+	RND_EVENT_DAD_NEW_DIALOG,         /* [a] called by the GUI after a new DAD dialog is open; args are pointer hid_ctx,  string dialog id and a pointer to int[4] for getting back preferre {x,y,w,h} (-1 means unknown) */
+	RND_EVENT_DAD_NEW_GEO,            /* [a] called by the GUI after the window geometry _may_ have changed; args are: void *hid_ctx, const char *dialog id, int x1, int y1, int width, int height */
 
-	RND_EVENT_EXPORT_SESSION_BEGIN,   /* called before an export session (e.g. CAM script execution) starts; should not be nested; there's no guarantee that options are parsed before or after this event */
-	RND_EVENT_EXPORT_SESSION_END,     /* called after an export session (e.g. CAM script execution) ends */
+	RND_EVENT_EXPORT_SESSION_BEGIN,   /* [a] called before an export session (e.g. CAM script execution) starts; should not be nested; there's no guarantee that options are parsed before or after this event */
+	RND_EVENT_EXPORT_SESSION_END,     /* [a] called after an export session (e.g. CAM script execution) ends */
 
-	RND_EVENT_STROKE_START,           /* parameters: rnd_coord_t x, rnd_coord_t y */
-	RND_EVENT_STROKE_RECORD,          /* parameters: rnd_coord_t x, rnd_coord_t y */
-	RND_EVENT_STROKE_FINISH,          /* parameters: int *handled; if it is non-zero, stroke has handled the request and Tool() should return 1, breaking action script execution */
+	RND_EVENT_STROKE_START,           /* [a] parameters: rnd_coord_t x, rnd_coord_t y */
+	RND_EVENT_STROKE_RECORD,          /* [a] parameters: rnd_coord_t x, rnd_coord_t y */
+	RND_EVENT_STROKE_FINISH,          /* [a] parameters: int *handled; if it is non-zero, stroke has handled the request and Tool() should return 1, breaking action script execution */
 
-	RND_EVENT_DESIGN_SET_CURRENT,     /* called after the current design on display got _replaced_; should be emitted when the design data got replaced in memory or GUI switched between designs loaded (in case of multiple designs); argument is rnd_design_t *now_active */
-	RND_EVENT_DESIGN_META_CHANGED,    /* called if the metadata of the design has changed (size, changed flag, etc; anything except the file names) */
-	RND_EVENT_DESIGN_FN_CHANGED,      /* called after the file name of the design has changed */
+	RND_EVENT_DESIGN_SET_CURRENT,     /* [d] called after the current design on display got _replaced_; should be emitted when the design data got replaced in memory or GUI switched between designs loaded (in case of multiple designs); argument is rnd_design_t *now_active */
+	RND_EVENT_DESIGN_META_CHANGED,    /* [d] called if the metadata of the design has changed (size, changed flag, etc; anything except the file names) */
+	RND_EVENT_DESIGN_FN_CHANGED,      /* [d] called after the file name of the design has changed */
 
-	RND_EVENT_SAVE_PRE,               /* called before saving the design (required for window placement) */
-	RND_EVENT_SAVE_POST,              /* called after saving the design (required for window placement) */
-	RND_EVENT_LOAD_PRE,               /* called before loading a new design (required for window placement) */
-	RND_EVENT_LOAD_POST,              /* called after loading a new design, whether it was successful or not (required for window placement) */
+	RND_EVENT_SAVE_PRE,               /* [d] called before saving the design (required for window placement) */
+	RND_EVENT_SAVE_POST,              /* [d] called after saving the design (required for window placement) */
+	RND_EVENT_LOAD_PRE,               /* [d] called before loading a new design (required for window placement) */
+	RND_EVENT_LOAD_POST,              /* [d] called after loading a new design, whether it was successful or not (required for window placement) */
 
-	RND_EVENT_MENU_CHANGED,           /* called after a menu merging (which means actual menu system change) */
-	RND_EVENT_GUI_BATCH_TIMER,        /* request timed batch GUI update, see rnd_hid_gui_batch_timer() */
-	RND_EVENT_MAINLOOP_CHANGE,        /* called after the mainloop variable has changed */
+	RND_EVENT_MENU_CHANGED,           /* [a] called after a menu merging (which means actual menu system change) */
+	RND_EVENT_GUI_BATCH_TIMER,        /* [a] request timed batch GUI update, see rnd_hid_gui_batch_timer() */
+	RND_EVENT_MAINLOOP_CHANGE,        /* [a] called after the mainloop variable has changed */
 
-	RND_EVENT_DESIGN_META_CHANGED_LIB,/* called by librnd as the earliest event when it changes the config of the design */
+	RND_EVENT_DESIGN_META_CHANGED_LIB,/* [d] called by librnd as the earliest event when it changes the config of the design */
 
-	RND_EVENT_DAD_NEW_PANE,           /* called by the GUI after a new paned widget is created; args are pointer hid_ctx, string dialog, string paned id and a (double *) for getting back preferred ratio (-1 means unknown) */
-	RND_EVENT_DAD_PANE_GEO_CHG,       /* called by the GUI after the pane geometry _may_ have changed; args are: void *hid_ctx, const char *dialog id, const char *pane id, double ratio */
+	RND_EVENT_DAD_NEW_PANE,           /* [a] called by the GUI after a new paned widget is created; args are pointer hid_ctx, string dialog, string paned id and a (double *) for getting back preferred ratio (-1 means unknown) */
+	RND_EVENT_DAD_PANE_GEO_CHG,       /* [a] called by the GUI after the pane geometry _may_ have changed; args are: void *hid_ctx, const char *dialog id, const char *pane id, double ratio */
 
-	RND_EVENT_CONF_FILE_SAVE_POST,    /* called after a stand-alone config/project file has been saved. Args: (const char *) file name, int role */
+	RND_EVENT_CONF_FILE_SAVE_POST,    /* [a] called after a stand-alone config/project file has been saved. Args: (const char *) file name, int role */
 
 	RND_EVENT_last                    /* not a real event */
 } rnd_event_id_t;
