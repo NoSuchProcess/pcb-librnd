edb031a15f679f461a7ba51392dcb44d44c00b67
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2022-12-15 04:00:25 +0000

    [core] -Cleanup: conf_multi is the final API now
    git-svn-id: svn://repo.hu/librnd/trunk@34917 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/conf_multi.h
+++ src/librnd/core/conf_multi.h
@@ -1,8 +1,30 @@
-/* Temporary API for pre-4.0.0 multi-sheet support in sch-rnd 
+/*
+ *                            COPYRIGHT
+ *
+ *  librnd, modular 2D CAD framework
+ *  Copyright (C) 2022 Tibor 'Igor2' Palinkas
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Contact:
+ *    Project page: http://repo.hu/projects/librnd
+ *    lead developer: http://repo.hu/projects/librnd/contact.html
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ */
 
-   DO NOT USE THIS API outside of sch-rnd.
-
-*/
+/* multi-design conf support */
 
 #include <genlist/gendlist.h>
 
