aaed57a159dda0ef5648956532f90ec391cbbf61
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-02-25 04:18:58 +0000

    [polybool] -Split: move out more pline code and dead code from polyarea
    git-svn-id: svn://repo.hu/librnd/trunk@35031 1d61de99-560f-4dba-8957-1e505d42358b

---
+++ src/librnd/polybool/pa_dead.c
@@ -0,0 +1,49 @@
+/*
+   based on:
+       poly_Boolean: a polygon clip library
+       Copyright (C) 1997  Alexey Nikitin, Michael Leonov
+       (also the authors of the paper describing the actual algorithm)
+       leonov@propro.iis.nsk.su
+
+   in turn based on:
+       nclip: a polygon clip library
+       Copyright (C) 1993  Klamer Schutte
+ 
+       This program is free software; you can redistribute it and/or
+       modify it under the terms of the GNU General Public
+       License as published by the Free Software Foundation; either
+       version 2 of the License, or (at your option) any later version.
+ 
+       This program is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+       General Public License for more details.
+ 
+       You should have received a copy of the GNU General Public
+       License along with this program; if not, write to the Free
+       Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ 
+      polygon1.c
+      (C) 1997 Alexey Nikitin, Michael Leonov
+      (C) 1993 Klamer Schutte
+
+      all cases where original (Klamer Schutte) code is present
+      are marked
+*/
+
+#if 0
+static rnd_bool poly_M_CheckInside(rnd_polyarea_t * p, rnd_vector_t v0)
+{
+	rnd_polyarea_t *cur;
+
+	if ((p == NULL) || (v0 == NULL))
+		return rnd_false;
+	cur = p;
+	do {
+		if (rnd_polyarea_contour_inside(cur, v0))
+			return rnd_true;
+	}
+	while ((cur = cur->f) != p);
+	return rnd_false;
+}
+#endif
