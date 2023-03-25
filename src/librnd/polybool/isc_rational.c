6d0f37e1c9ea0599735e570664d19e6a5f359a88
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-03-25 12:49:22 +0000

    [polybool] -Split: isc_rational's types and API into a .h
    git-svn-id: svn://repo.hu/librnd/trunk@35325 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/polybool/isc_rational.c
+++ src/librnd/polybool/isc_rational.c
@@ -40,29 +40,10 @@
 #define RATIONAL_API   RND_INLINE
 #define RATIONAL_IMPL  RND_INLINE
 
-#	if RND_COORD_MAX == ((1UL<<31)-1)
-#		define BIG_BITS 32
-#		define BIG_DECIMAL_DIGITS 9
-#		define BIG_DECIMAL_BASE 1000000000UL
-#		define BIG_NEG_BASE 0x80000000UL
-#	elif RND_COORD_MAX == ((1ULL<<63)-1)
-#		define BIG_BITS 64
-#		define BIG_DECIMAL_DIGITS 19
-#		define BIG_DECIMAL_BASE 10000000000000000000UL
-#		define BIG_NEG_BASE 0x8000000000000000UL
-#	else
-#		error "unsupported system: rnd_coord has to be 32 or 64 bits wide (checked: RND_COORD_MAX)"
-#	endif
+#define FROM_ISC_RATIONAL_C
 
-#define RND_BIGCRD_WIDTH 3
 typedef rnd_coord_t big_word;
-typedef rnd_coord_t rnd_big_coord_t[RND_BIGCRD_WIDTH];
 
-#include <genfip/big.h>
-
-
-#define RATIONAL(x) rnd_bcr_ ## x
-#define RATIONAL_INT rnd_big_coord_t
 
 #define RATIONAL_OP_ADD(r,a,b) big_addn((r), (a), (b), RND_BIGCRD_WIDTH, 0)
 #define RATIONAL_OP_SUB(r,a,b) big_subn((r), (a), (b), RND_BIGCRD_WIDTH, 0)
@@ -85,14 +66,12 @@ do { \
 #define RATIONAL_OP_SWAP(a, b) (big_swap((a), (b), RND_BIGCRD_WIDTH))
 #define RATIONAL_OP_CPY(dst, src) big_copy(*(dst), *(src), RND_BIGCRD_WIDTH)
 
-#include <genfip/rational.h>
+#include "isc_rational.h"
 
 /*** implementation ***/
 
 #define W RND_BIGCRD_WIDTH
 
-/* Single intersection between non-parallel lines x1;y1->x2;y2 and x3;y3->x4;y4.
-   Returns 0 on success and loads x;y with the coords of the intersection */
 int rnd_big_coord_isc(rnd_bcr_t *x, rnd_bcr_t *y, rnd_coord_t x1, rnd_coord_t y1, rnd_coord_t x2, rnd_coord_t y2, rnd_coord_t x3, rnd_coord_t y3, rnd_coord_t x4, rnd_coord_t y4)
 {
 	rnd_big_coord_t tmp1, tmp2, dx1, dy1, dx3, dy3, a, b, denom;
---
+++ src/librnd/polybool/isc_rational.h
@@ -0,0 +1,31 @@
+#	if RND_COORD_MAX == ((1UL<<31)-1)
+#		define BIG_BITS 32
+#		define BIG_DECIMAL_DIGITS 9
+#		define BIG_DECIMAL_BASE 1000000000UL
+#		define BIG_NEG_BASE 0x80000000UL
+#	elif RND_COORD_MAX == ((1ULL<<63)-1)
+#		define BIG_BITS 64
+#		define BIG_DECIMAL_DIGITS 19
+#		define BIG_DECIMAL_BASE 10000000000000000000UL
+#		define BIG_NEG_BASE 0x8000000000000000UL
+#	else
+#		error "unsupported system: rnd_coord has to be 32 or 64 bits wide (checked: RND_COORD_MAX)"
+#	endif
+
+#define RND_BIGCRD_WIDTH 3
+typedef rnd_coord_t rnd_big_coord_t[RND_BIGCRD_WIDTH];
+
+#define RATIONAL(x) rnd_bcr_ ## x
+#define RATIONAL_INT rnd_big_coord_t
+
+#ifndef FROM_ISC_RATIONAL_C
+#define RATIONAL_INHIBIT_IMPLEMENTATION
+#else
+#include <genfip/big.h>
+#endif
+
+#include <genfip/rational.h>
+
+/* Single intersection between non-parallel lines x1;y1->x2;y2 and x3;y3->x4;y4.
+   Returns 0 on success and loads x;y with the coords of the intersection */
+int rnd_big_coord_isc(rnd_bcr_t *x, rnd_bcr_t *y, rnd_coord_t x1, rnd_coord_t y1, rnd_coord_t x2, rnd_coord_t y2, rnd_coord_t x3, rnd_coord_t y3, rnd_coord_t x4, rnd_coord_t y4);
