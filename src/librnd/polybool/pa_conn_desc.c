0713a6a32b780ed19b32f8acbe61ebad9673b5f4
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-04-05 15:09:12 +0000

    [polybool] -Split: angle calculation code from conn desc allocation so it can be replaced with big-angle calculation later
    git-svn-id: svn://repo.hu/librnd/trunk@35392 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/polybool/pa_conn_desc.c
+++ src/librnd/polybool/pa_conn_desc.c
@@ -53,10 +53,35 @@ static pa_conn_desc_t *pa_prealloc_conn_desc(pa_big_vector_t isc)
 	return cd;
 }
 
+RND_INLINE void pa_conn_calc_angle_small(pa_conn_desc_t *cd, rnd_vnode_t *pt, char poly, char side, rnd_vector_t v)
+{
+	double ang, dx, dy;
+
+	assert(!Vequ2(v, rnd_vect_zero));
+
+	dx = fabs((double)v[0]);
+	dy = fabs((double)v[1]);
+	ang = dy / (dy + dx);
+
+	/* now move to the actual quadrant */
+	if ((v[0] < 0) && (v[1] >= 0))         cd->angle = 2.0 - ang; /* 2nd quadrant */
+	else if ((v[0] < 0) && (v[1] < 0))     cd->angle = 2.0 + ang; /* 3rd quadrant */
+	else if ((v[0] >= 0) && (v[1] < 0))    cd->angle = 4.0 - ang; /* 4th quadrant */
+	else                                   cd->angle = ang;       /* 1st quadrant */
+
+	assert((ang >= 0.0) && (ang <= 4.0));
+
+	DEBUG_ANGLE("point on %c at %$mD assigned angle %.08f on side %c\n", poly, pt->point[0], pt->point[1], ang, side);
+}
+
+RND_INLINE void pa_conn_calc_angle(pa_conn_desc_t *cd, rnd_vnode_t *pt, char poly, char side, rnd_vector_t v)
+{
+	pa_conn_calc_angle_small(cd, pt, poly, side, v);
+}
+
 static pa_conn_desc_t *pa_new_conn_desc(pa_conn_desc_t *cd, rnd_vnode_t *pt, char poly, char side)
 {
 	rnd_vector_t v;
-	double ang, dx, dy;
 
 	cd->head = NULL;
 	cd->parent = pt;
@@ -94,22 +119,7 @@ static pa_conn_desc_t *pa_new_conn_desc(pa_conn_desc_t *cd, rnd_vnode_t *pt, cha
 		}
 	}
 
-	assert(!Vequ2(v, rnd_vect_zero));
-
-	dx = fabs((double)v[0]);
-	dy = fabs((double)v[1]);
-	ang = dy / (dy + dx);
-
-	/* now move to the actual quadrant */
-	if ((v[0] < 0) && (v[1] >= 0))         cd->angle = 2.0 - ang; /* 2nd quadrant */
-	else if ((v[0] < 0) && (v[1] < 0))     cd->angle = 2.0 + ang; /* 3rd quadrant */
-	else if ((v[0] >= 0) && (v[1] < 0))    cd->angle = 4.0 - ang; /* 4th quadrant */
-	else                                   cd->angle = ang;       /* 1st quadrant */
-
-	assert((ang >= 0.0) && (ang <= 4.0));
-
-	DEBUG_ANGLE("point on %c at %$mD assigned angle %.08f on side %c\n", poly, pt->point[0], pt->point[1], ang, side);
-
+	pa_conn_calc_angle(cd, pt, poly, side, v);
 
 	return cd;
 }
