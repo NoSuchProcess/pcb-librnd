7624aa0953a1625128f09a415ec70fac8711856d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-05-08 08:43:59 +0000

    [core] -Add: SafeFs actions for querying file stat()s, mirroring discrete calls of safe_fs.h
    git-svn-id: svn://repo.hu/librnd/trunk@35484 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/safe_fs_act.c
+++ src/librnd/core/safe_fs_act.c
@@ -116,13 +116,55 @@ static fgw_error_t rnd_act_SafeFsRename(fgw_arg_t *res, int argc, fgw_arg_t *arg
 }
 
 
+static const char rnd_acts_SafeFsFileSize[] = "SafeFsFileSize(path)";
+static const char rnd_acth_SafeFsFileSize[] = "Return the size of a file in bytes, or -1 on error.";
+static fgw_error_t rnd_act_SafeFsFileSize(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+{
+	const char *path;
+
+	RND_ACT_CONVARG(1, FGW_STR, SafeFsFileSize, path = argv[1].val.str);
+
+	res->type = FGW_LONG;
+	res->val.nat_long = rnd_file_mtime(RND_ACT_DESIGN, path);
+	return 0;
+}
+
+static const char rnd_acts_SafeFsFileMtime[] = "SafeFsFileMtime(path)";
+static const char rnd_acth_SafeFsFileMtime[] = "Return the last modification time of a file, from Epoch, or -1 on error.";
+static fgw_error_t rnd_act_SafeFsFileMtime(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+{
+	const char *path;
+
+	RND_ACT_CONVARG(1, FGW_STR, SafeFsFileMtime, path = argv[1].val.str);
+
+	res->type = FGW_DOUBLE;
+	res->val.nat_double = rnd_file_mtime(RND_ACT_DESIGN, path);
+	return 0;
+}
+
+
+static const char rnd_acts_SafeFsIsDir[] = "SafeFsIsDir(path)";
+static const char rnd_acth_SafeFsIsDir[] = "Return 1 if path exists and is a directory, else return 0.";
+static fgw_error_t rnd_act_SafeFsIsDir(fgw_arg_t *res, int argc, fgw_arg_t *argv)
+{
+	const char *path;
+
+	RND_ACT_CONVARG(1, FGW_STR, SafeFsIsDir, path = argv[1].val.str);
+
+	res->type = FGW_INT;
+	res->val.nat_int = rnd_is_dir(RND_ACT_DESIGN, path);
+	return 0;
+}
 
 static rnd_action_t rnd_safe_fs_action_list[] = {
 	{"SafeFsSystem", rnd_act_SafeFsSystem, rnd_acth_SafeFsSystem, rnd_acts_SafeFsSystem},
 	{"SafeFsRemove", rnd_act_SafeFsRemove, rnd_acth_SafeFsRemove, rnd_acts_SafeFsRemove},
 	{"SafeFsUnlink", rnd_act_SafeFsUnlink, rnd_acth_SafeFsUnlink, rnd_acts_SafeFsUnlink},
 	{"SafeFsMkdir", rnd_act_SafeFsMkdir, rnd_acth_SafeFsMkdir, rnd_acts_SafeFsMkdir},
-	{"SafeFsRename", rnd_act_SafeFsRename, rnd_acth_SafeFsRename, rnd_acts_SafeFsRename}
+	{"SafeFsRename", rnd_act_SafeFsRename, rnd_acth_SafeFsRename, rnd_acts_SafeFsRename},
+	{"SafeFsFileSize", rnd_act_SafeFsFileSize, rnd_acth_SafeFsFileSize, rnd_acts_SafeFsFileSize},
+	{"SafeFsFileMtime", rnd_act_SafeFsFileMtime, rnd_acth_SafeFsFileMtime, rnd_acts_SafeFsFileMtime},
+	{"SafeFsIsDir", rnd_act_SafeFsIsDir, rnd_acth_SafeFsIsDir, rnd_acts_SafeFsIsDir}
 };
 
 void rnd_safe_fs_act_init2(void)
