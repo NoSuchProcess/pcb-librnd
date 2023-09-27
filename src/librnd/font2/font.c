be606805ff9e50d7b827db6cabe89e61b822a2c4
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-09-27 10:44:42 +0000

    [font2] -Fix: more accurate warning text on missing tab_width
    git-svn-id: svn://repo.hu/librnd/trunk@35623 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/font2/font.c
+++ src/librnd/font2/font.c
@@ -56,7 +56,7 @@ RND_INLINE rnd_coord_t rnd_font_advance_tab(rnd_font_t *font, rnd_font_render_op
 	rnd_coord_t tabsize;
 
 	if ((font->tab_width_cache <= 0) && (font->tab_width <= 0))
-		rnd_trace("missing tab width\n");
+		rnd_trace("librnd font: missing tab_width in font; improvising a value\n");
 
 	if (font->tab_width_cache <= 0)
 		font->tab_width_cache = font->tab_width;
