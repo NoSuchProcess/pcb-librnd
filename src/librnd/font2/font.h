b1ff1775835e260f4edafff7df534849aed2aeba
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-09-28 03:33:22 +0000

    [font2] -Add: extend the rnd_font_render_opts_t enum with a multiline option
    git-svn-id: svn://repo.hu/librnd/trunk@35643 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/font2/font.h
+++ src/librnd/font2/font.h
@@ -80,7 +80,8 @@ typedef enum {                 /* bitfield - order matters for backward compatib
 	RND_FONT_MIRROR_Y = 1,       /* change Y coords (mirror over the X axis) */
 	RND_FONT_MIRROR_X = 2,       /* change X coords (mirror over the Y axis) */
 	RND_FONT_HTAB = 4,           /* render horizontal tab */
-	RND_FONT_ENTITY = 8          /* interpret &entity; sequences and render them single glyph */
+	RND_FONT_ENTITY = 8,         /* interpret &entity; sequences and render them single glyph */
+	RND_FONT_MULTI_LINE = 16     /* support rendering into multiple lines (split at \n) */
 } rnd_font_render_opts_t;
 
 typedef enum rnd_font_tiny_e { /* How to draw text that is too tiny to be readable */
