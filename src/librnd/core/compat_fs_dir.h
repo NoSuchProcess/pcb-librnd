e80e9c4d82351f4230bd1eba4d47e4907d9e693d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2023-09-30 02:07:51 +0000

    -Fix: mark API avaialble only from 4.1.0
    git-svn-id: svn://repo.hu/librnd/trunk@35678 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/core/compat_fs_dir.h
+++ src/librnd/core/compat_fs_dir.h
@@ -1,6 +1,6 @@
 #include <genvector/gds_char.h>
 #include <librnd/core/hidlib.h>
 
-/* Safe temp dir creation in /tmp */
+/* [4.1.0] Safe temp dir creation in /tmp */
 int rnd_mktempdir(rnd_design_t *dsg, gds_t *dst, const char *prefix);
 int rnd_rmtempdir(rnd_design_t *dsg, gds_t *dst);
