2c03018c86fbd414970e42728d54cf5a1b25b898
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-01-26 10:49:50 +0000

    -Fix: missing include
    git-svn-id: svn://repo.hu/librnd/trunk@36411 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/plugins/lib_hid_common/dlg_export.h
+++ src/librnd/plugins/lib_hid_common/dlg_export.h
@@ -10,6 +10,6 @@ fgw_error_t rnd_act_PrintDialog(fgw_arg_t *ores, int oargc, fgw_arg_t *oargv);
    actioins, e.g. for exporting a whole project */
 void rnd_dlg_export(const char *title, int exporters, int printers, rnd_design_t *dsg, void *appspec);
 
-
+#include <librnd/core/event.h>
 void rnd_dialog_export_close(rnd_design_t *hidlib, void *user_data, int argc, rnd_event_arg_t argv[]);
 
