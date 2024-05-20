4872da957b5cc3a164c38727d7a222087f2ce36d
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-05-20 13:29:06 +0000

    [polybool2] -Add: rounded offset: figure if a corner is convec or concave
    git-svn-id: svn://repo.hu/librnd/trunk@37233 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/polybool2/offset2.c
+++ src/librnd/polybool2/offset2.c
@@ -23,13 +23,15 @@ RND_INLINE rnd_vnode_t *pl_append_xy(rnd_pline_t *dst, rnd_coord_t x, rnd_coord_
 
 rnd_pline_t *rnd_pline_dup_with_offset_round(const rnd_pline_t *src, rnd_coord_t offs)
 {
-	const rnd_vnode_t *curr;
+	const rnd_vnode_t *curr, *next;
 	rnd_vnode_t *vnew;
 	rnd_pline_t *dst = calloc(sizeof(rnd_pline_t), 1);
+	rnd_vector_t isc1, isc2;
 
 	if (dst == NULL)
 		return NULL;
 
+	/* offset each edge also keeping the original corners but marked */
 	curr = src->head;
 	do {
 		double vx, vy, nx, ny, len;
@@ -53,6 +55,36 @@ rnd_pline_t *rnd_pline_dup_with_offset_round(const rnd_pline_t *src, rnd_coord_t
 		pl_append_xy(dst, curr->point[0] + nx, curr->point[1] + ny);
 		pl_append_xy(dst, curr->next->point[0] + nx, curr->next->point[1] + ny);
 	} while((curr = curr->next) != src->head);
+
+
+	/* the next section doesn't work correctly when starting on a marked node
+	   (because of all the node removals) */
+	if (dst->head->flg.mark)
+		dst->head = dst->head->next;
+
+	/* visit original corners (marked) and check if they are now the center of
+	   a round outer corner (convex) or an external point on an inverted corner
+	   (concave) */
+	curr = dst->head;
+	do {
+		next = curr->next;
+
+		if (curr->flg.mark) {
+			if (rnd_vect_inters2(curr->prev->prev->point, curr->prev->point, curr->next->point, curr->next->next->point, isc1, isc2) == 1) {
+				/* prev and next seg intersect -> concave */
+				rnd_poly_vertex_exclude(dst, (rnd_vnode_t *)curr);
+				rnd_poly_vertex_exclude(dst, (rnd_vnode_t *)curr->prev);
+				curr->next->point[0] = isc1[0];
+				curr->next->point[1] = isc1[1];
+			}
+			else {
+				/* convex: add a rounded corner */
+				rnd_poly_vertex_exclude(dst, (rnd_vnode_t *)curr);
+			}
+		}
+
+	} while((curr = next) != dst->head);
+
 
 	pa_pline_update(dst, 1);
 	return dst;
