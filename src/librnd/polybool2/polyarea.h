82933de9b48515efc3ec412d636307a5dced2431
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-09-08 03:52:50 +0000

    [polybool2] -Cleanup: rewrite polyarea.h copyright banner, keeping copyright reference of the original
    git-svn-id: svn://repo.hu/librnd/trunk@37363 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/polybool2/polyarea.h
+++ src/librnd/polybool2/polyarea.h
@@ -1,26 +1,38 @@
 /*
-      poly_Boolean: a polygon clip library
-      Copyright (C) 1997  Alexey Nikitin, Michael Leonov
-      leonov@propro.iis.nsk.su
-
-      This library is free software; you can redistribute it and/or
-      modify it under the terms of the GNU Library General Public
-      License as published by the Free Software Foundation; either
-      version 2 of the License, or (at your option) any later version.
-
-      This library is distributed in the hope that it will be useful,
-      but WITHOUT ANY WARRANTY; without even the implied warranty of
-      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-      Library General Public License for more details.
-
-      You should have received a copy of the GNU Library General Public
-      License along with this library; if not, write to the Free
-      Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-      polyarea.h
-      (C) 1997 Alexey Nikitin, Michael Leonov
-      (C) 1997 Klamer Schutte (minor patches)
-*/
+ *                            COPYRIGHT
+ *
+ *  libpolybool, 2D polygon bool operations
+ *  Copyright (C) 2024 Tibor 'Igor2' Palinkas
+ *
+ *  (Supported by NLnet NGI0 Entrust in 2024)
+ *
+ *  Far descendant of poly_Boolean: a polygon clip library
+ *    Copyright (C) 1997  Alexey Nikitin, Michael Leonov
+ *    leonov@propro.iis.nsk.su
+ *    polyarea.h:
+ *    (C) 1997 Alexey Nikitin, Michael Leonov
+ *    (C) 1997 Klamer Schutte (minor patches)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.*
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  Contact:
+ *    Project page: http://www.repo.hu/projects/librnd
+ *    lead developer: http://www.repo.hu/projects/librnd/contact.html
+ *    mailing list: pcb-rnd (at) list.repo.hu (send "subscribe")
+ *
+ */
 
 #ifndef RND_POLYAREA_H
 #define RND_POLYAREA_H
