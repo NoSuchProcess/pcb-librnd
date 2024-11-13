024f26643af6d3334e830a0193c9a0d1df7e117e
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-11-13 03:18:00 +0000

    -Update: bump version to 4.3.2-dev
    git-svn-id: svn://repo.hu/librnd/trunk@37516 1d61de99-560f-4dba-8957-1e505d42358b

--- scconfig/hooks.c
+++ scconfig/hooks.c
@@ -11,7 +11,7 @@
 #include "util/arg_auto_set.h"
 #include "Rev.h"
 
-#define version "4.3.1"
+#define version "4.3.2-dev"
 
 #define LIBRND_ROOT         "../src/librnd"
 #define LIBRND_PLUGIN_ROOT  "../src/librnd/plugins"
