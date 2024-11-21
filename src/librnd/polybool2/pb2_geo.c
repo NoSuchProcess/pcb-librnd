NoSuchProcess/pcb-librnd

commit 18e8e4a60d43dd4355d9e5bd3a85962a2c35419c

[polybool2] -Add: extend full overlap test for the arc-arc case
git-svn-id: svn://repo.hu/librnd/trunk@37592 1d61de99-560f-4dba-8957-1e505d42358b

src/librnd/polybool2/pb2_geo.c
@@ -42,12 +42,27 @@ TODO("bignum: calc_t should be bignum, not double, especially with 64 bit coords
 /* returns 1 if sa and sb are in full overlap */
 RND_INLINE int seg_seg_olap(pb2_seg_t *sa, pb2_seg_t *sb)
 {
-	TODO("arc: this is only for line at the moment");
-	if (Vequ2(sa->start, sb->start) && Vequ2(sa->end, sb->end))
-		return 1;
-	if (Vequ2(sa->start, sb->end) && Vequ2(sa->end, sb->start))
-		return 1;
-	return 0;
+	if (sa->shape_type != sb->shape_type)
+		return 0;
+
+	/* endpoints must match, regardless of the shape */
+	if (!Vequ2(sa->start, sb->start) || !Vequ2(sa->end, sb->end))
+		return 0;
+	if (!Vequ2(sa->start, sb->end) || !Vequ2(sa->end, sb->start))
+		return 0;
+
+	switch(sa->shape_type) {
+		case RND_VNODE_LINE: break; /*matching endpoints is enough to check */
+		case RND_VNODE_ARC:
+			/* need to have same endpoints, same center */
+			if (fabs(sa->shape.arc.cx - sb->shape.arc.cx) > 0.5)
+				return 0;
+			if (fabs(sa->shape.arc.cy - sb->shape.arc.cy) > 0.5)
+				return 0;
+			break;
+	}
+
+	return 1;
 }
 
 /* swap start and end of a segment (reverse orientation); rtree doesn't change */

commit af3cfa489d5dd07a169910e02ff4d1e9d108b4b8

[polybool2] -Fix: bug introduced in r37592 in endpoint matching logics for full-overlap test
git-svn-id: svn://repo.hu/librnd/trunk@37602 1d61de99-560f-4dba-8957-1e505d42358b

src/librnd/polybool2/pb2_geo.c
@@ -38,6 +38,14 @@ TODO("bignum: calc_t should be bignum, not double, especially with 64 bit coords
 #include <gengeo2d/carc.h>
 #include <gengeo2d/intersect.h>
 
+RND_INLINE int seg_seg_end_match(pb2_seg_t *sa, pb2_seg_t *sb)
+{
+	if (Vequ2(sa->start, sb->start) && Vequ2(sa->end, sb->end))
+		return 1;
+	if (Vequ2(sa->start, sb->end) && Vequ2(sa->end, sb->start))
+		return 1;
+	return 0;
+}
 
 /* returns 1 if sa and sb are in full overlap */
 RND_INLINE int seg_seg_olap(pb2_seg_t *sa, pb2_seg_t *sb)
@@ -46,9 +54,7 @@ RND_INLINE int seg_seg_olap(pb2_seg_t *sa, pb2_seg_t *sb)
 		return 0;
 
 	/* endpoints must match, regardless of the shape */
-	if (!Vequ2(sa->start, sb->start) || !Vequ2(sa->end, sb->end))
-		return 0;
-	if (!Vequ2(sa->start, sb->end) || !Vequ2(sa->end, sb->start))
+	if (!seg_seg_end_match(sa, sb))
 		return 0;
 
 	switch(sa->shape_type) {