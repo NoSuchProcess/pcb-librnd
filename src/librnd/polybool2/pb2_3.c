8cd35e5f7d765c19539a8ac754ae4d8ff6f66d45
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-11-25 08:32:03 +0000

    [polybool2] -Fix: step 3, arc support: handle the case that a segment slope leaves endpoint to the left but ray starting point starts to the right - no intersection possible
    git-svn-id: svn://repo.hu/librnd/trunk@37643 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/polybool2/pb2_3.c
+++ src/librnd/polybool2/pb2_3.c
@@ -112,6 +112,11 @@ RND_INLINE void pb2_3_fp_at_endp(pb2_3_face_polarity_t *pctx, rnd_vector_t p, rn
 			return;
 		}
 
+		if ((slp_seg.dx < 0) && (pctx->dir_slope.dx > 0)) {
+			if (pb2_face_polarity_at_verbose) pa_trace("   slope dir: seg going left, pt dir going right\n", 0);
+			return;
+		}
+
 		if (pctx->dir[1] < 0) { /* dir pointing up */
 			if ((slp_seg.s < 0) || PB2_SLOPE_LT(pctx->dir_slope, slp_seg)) {
 				pb2_3_seg_hit(pctx, seg);
