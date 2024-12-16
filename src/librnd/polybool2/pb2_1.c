43630730883ce5260f40992bb3b47f75e7d9ff1b
igor2 <igor2@1d61de99-560f-4dba-8957-1e505d42358b>
2024-12-16 04:30:30 +0000

    [polybool2] -Optimize: arc step 1: don't recurse and don't compute arc bbox/angles twice when an arc does not need split for vcrossing
    git-svn-id: svn://repo.hu/librnd/trunk@37781 1d61de99-560f-4dba-8957-1e505d42358b

--- src/librnd/polybool2/pb2_1.c
+++ src/librnd/polybool2/pb2_1.c
@@ -575,6 +575,14 @@ RND_INLINE void pb2_1_map_seg_arc_(pb2_ctx_t *ctx, const rnd_vector_t p1, const 
 		sect[num_sects].ang = sa+da;
 		num_sects++;
 
+		if (num_sects == 2) {
+			/* did not have to split, simply use already existing ictx.seg (this
+			   avoids an unnecessary recursion and pb2_seg_arc_update_cache() call) */
+			assert(Vequ2(sect[0].pt, p1));
+			assert(Vequ2(sect[1].pt, p2));
+			goto intact;
+		}
+
 		if (num_sects == 4) {
 			int need_swap = 0;
 			/* make sure top and bottom are ordered from start to end; this is the
@@ -591,16 +599,19 @@ RND_INLINE void pb2_1_map_seg_arc_(pb2_ctx_t *ctx, const rnd_vector_t p1, const 
 			}
 		}
 
+
+		/* need to split into 2 or 3 arcs */
 		assert(num_sects > 1);
 		assert(num_sects < 5);
+
 		if (num_sects > 1) pb2_1_map_seg_arc_(ctx, sect[0].pt, sect[1].pt, center, adir, poly_id, 0);
 		if (num_sects > 2) pb2_1_map_seg_arc_(ctx, sect[1].pt, sect[2].pt, center, adir, poly_id, 0);
 		if (num_sects > 3) pb2_1_map_seg_arc_(ctx, sect[2].pt, sect[3].pt, center, adir, poly_id, 0);
 		return;
 	}
 
+	intact:;
 /*rnd_trace("arc add: %f..%f: %ld;%ld -> %ld;%ld\n", ictx.seg.shape.arc.start, ictx.seg.shape.arc.start + ictx.seg.shape.arc.delta, p1[0], p1[1], p2[0], p2[1]);*/
-
 	pb2_arc_bbox(&ictx.seg);
 	pb2_1_map_any(ctx, &ictx, poly_id);
 }
